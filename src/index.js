7f3f5714890ef3f093b2d42c2a693ee68e041d36
Florian Liesegang <lysigk@gmail.com>
2017-08-15 10:05:56 +0200

    Create basic App component

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+
+ReactDOM.render(<App onClick={console.log("Test")}/>, document.getElementById('root'));
 registerServiceWorker();
