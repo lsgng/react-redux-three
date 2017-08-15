acf1ac361835d03324345921dda8ee6a7000e9c0
Florian Liesegang <lysigk@gmail.com>
2017-08-15 11:06:08 +0200

    Move runThreeApp() into own module

--- src/index.js
+++ src/index.js
@@ -2,37 +2,9 @@ import './index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as THREE from 'three';
 
 import App from './App';
 
-const init = () => {
-    console.log('init')
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+import runThreeApp from './runThreeApp';
 
-    const container = document.getElementById('container');
-    const renderer = new THREE.WebGLRenderer({antialias: true});
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    container.appendChild(renderer.domElement);
-
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-
-    camera.position.z = 5;
-
-    const animate = function () {
-        requestAnimationFrame(animate);
-
-        cube.rotation.x += 0.1;
-        cube.rotation.y += 0.1;
-
-        renderer.render(scene, camera);
-    };
-
-    animate();
-};
-
-ReactDOM.render(<App init={init} />, document.getElementById('root'));
+ReactDOM.render(<App run={runThreeApp} />, document.getElementById('root'));
