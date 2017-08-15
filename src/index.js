53ff1fbae2193ea877ba71d44d60dbebf3e7dc6a
Florian Liesegang <lysigk@gmail.com>
2017-08-15 10:06:41 +0200

    Cleanup

--- src/index.js
+++ src/index.js
@@ -2,11 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-
-
-
 
 ReactDOM.render(<App onClick={console.log("Test")}/>, document.getElementById('root'));
-registerServiceWorker();
