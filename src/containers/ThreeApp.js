977e51a04ae1fad794bfe78d11fa47fa0d7a37d2
Florian Liesegang <lysigk@gmail.com>
2017-08-16 23:26:18 +0200

    Cleanup

--- src/containers/ThreeApp.js
+++ src/containers/ThreeApp.js
@@ -5,7 +5,7 @@ import { connect } from 'react-redux'
 
 import ThreeDisplay from '../components/ThreeDisplay'
 
-const ThreeApp = ({ running, run }) => (
+const ThreeApp = ({ running }) => (
     <div>
         <ThreeDisplay running={running} />
     </div>
