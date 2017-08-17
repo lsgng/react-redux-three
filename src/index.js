63f4e5b11f08646f9715b6488c7746650ebd5f00
Florian Liesegang <lysigk@gmail.com>
2017-08-17 11:15:32 +0200

    Fix render loop

--- src/index.js
+++ src/index.js
@@ -26,18 +26,17 @@ render(
     document.getElementById('root')
 )
 
-const updateThreeApp = () => {
-    const timestamp = Date.now()
-    requestAnimationFrame(() => store.dispatch({ type: 'UPDATE', timestamp: timestamp }))
-}
-
 const container = document.getElementById('container');
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setClearColor(0xf0ffff)
 renderer.setSize(window.innerWidth, window.innerHeight);
 container.appendChild(renderer.domElement);
 
-renderer.render(store.getState().scene, store.getState().camera)
+const updateThreeApp = () => {
+    renderer.render(store.getState().scene, store.getState().camera)
+    const timestamp = Date.now()
+    requestAnimationFrame(() => store.dispatch({ type: 'UPDATE', timestamp: timestamp }))
+}
 
 store.subscribe(updateThreeApp)
 
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,6 +1,6 @@
 const rotate = (state) => {
     const newState = { ...state }
-    newState.scene.getObjectByName('box').position.y = 2 * Math.sin(Date.now() / 10)
+    newState.scene.getObjectByName('box').position.y = 2 * Math.sin(Date.now() / 1000)
     return newState
 }
 
