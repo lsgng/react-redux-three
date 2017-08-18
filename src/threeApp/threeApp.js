67d62fc03a0d9fa2a940f8aaf2852bcca52c9cce
Florian Liesegang <lysigk@gmail.com>
2017-08-18 15:38:11 +0200

    Cleanup

--- src/threeApp/threeApp.js
+++ src/threeApp/threeApp.js
@@ -4,14 +4,15 @@ export const getThreeInitialState = () => {
     const scene = new THREE.Scene()
 
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-    camera.position.x = 40 
-    camera.position.y = 40 
+    camera.position.x = 40
+    camera.position.y = 40
     camera.position.z = 60
 
     for (let y = 0; y < 20; y++) {
         for (let x = 0; x < 20; x++) {
             const geometry = new THREE.SphereGeometry(1, 32, 32)
-            const material = new THREE.MeshBasicMaterial({ color: 0xffa500 })
+            const material = new THREE.MeshBasicMaterial()
+
             const sphere = new THREE.Mesh(geometry, material)
             sphere.position.x = x * 4
             sphere.position.y = y * 4
