146de4734c68c2c2052f7135fcb471b122c97382
Florian Liesegang <lysigk@gmail.com>
2017-08-18 11:52:57 +0200

    Replace single cube by grid of spheres

--- src/threeApp/threeActionHelpers.js
+++ src/threeApp/threeActionHelpers.js
@@ -1,11 +1,16 @@
 export const changeMaterial = (state) => {
     const newState = { ...state }
-    newState.scene.getObjectByName('box').material.wireframe = !newState.scene.getObjectByName('box').material.wireframe
+    const newChildren = state.scene.children.map((child) => {
+        child.material.color.r = Math.random()
+        child.material.color.b = Math.random()
+        return child
+    })
+    newState.children = newChildren
     return newState
 }
 
 export const rotate = (state) => {
     const newState = { ...state }
-    newState.scene.getObjectByName('box').position.y = 2 * Math.sin(Date.now() / 1000)
+    //newState.scene.getObjectByName('box').position.y = 2 * Math.sin(Date.now() / 1000)
     return newState
 }
--- src/threeApp/threeApp.js
+++ src/threeApp/threeApp.js
@@ -4,14 +4,20 @@ export const getThreeInitialState = () => {
     const scene = new THREE.Scene()
 
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-    camera.position.z = 5
+    camera.position.x = 40 
+    camera.position.y = 40 
+    camera.position.z = 60
 
-    const geometry = new THREE.BoxGeometry(1, 1, 1)
-    const material = new THREE.MeshBasicMaterial({ color: 0xffa500 })
-    const box = new THREE.Mesh(geometry, material)
-    box.name = 'box'
-    scene.add(box)
-
+    for (let y = 0; y < 20; y++) {
+        for (let x = 0; x < 20; x++) {
+            const geometry = new THREE.SphereGeometry(1, 32, 32)
+            const material = new THREE.MeshBasicMaterial({ color: 0xffa500 })
+            const sphere = new THREE.Mesh(geometry, material)
+            sphere.position.x = x * 4
+            sphere.position.y = y * 4
+            scene.add(sphere)
+        }
+    }
     return { scene, camera }
 }
 
