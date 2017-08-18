aaa491499aee5641b71d4e66512e771d03b8c5cc
Florian Liesegang <lysigk@gmail.com>
2017-08-18 15:42:01 +0200

    Cleanup

--- src/threeApp/threeApp.js
+++ src/threeApp/threeApp.js
@@ -12,7 +12,6 @@ export const getThreeInitialState = () => {
         for (let x = 0; x < 20; x++) {
             const geometry = new THREE.SphereGeometry(1, 32, 32)
             const material = new THREE.MeshBasicMaterial({ color: 0xffa500 })
-
             const sphere = new THREE.Mesh(geometry, material)
             sphere.position.x = x * 4
             sphere.position.y = y * 4
